83f5732a2733ebf8159f1617486305f6d98b445b
Buğra Öner <boletroy@gmail.com>
2022-05-28 01:06:15 +0300

    d

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -12,16 +12,16 @@ import Carousel from 'react-native-snap-carousel';
 import Feather from 'react-native-vector-icons/Feather';
 
 import BannerSlider from '../components/BannerSlider';
-import {windowWidth} from '../utils/Dimensions';
+import { windowWidth } from '../utils/Dimensions';
 
-import {freeGames, paidGames, sliderData} from '../model/data';
+import { freeGames, paidGames, sliderData } from '../model/data';
 import CustomSwitch from '../components/CustomSwitch';
 import ListItem from '../components/ListItem';
 
-export default function HomeScreen({navigation}) {
+export default function HomeScreen({ navigation }) {
   const [gamesTab, setGamesTab] = useState(1);
 
-  const renderBanner = ({item, index}) => {
+  const renderBanner = ({ item, index }) => {
     return <BannerSlider data={item} />;
   };
 
@@ -30,22 +30,22 @@ export default function HomeScreen({navigation}) {
   };
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-      <ScrollView style={{padding: 20}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ScrollView style={{ padding: 20 }}>
         <View
           style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
             marginBottom: 20,
           }}>
-          <Text style={{fontSize: 18, fontFamily: 'Roboto-Medium'}}>
-            Hello John Doe
+          <Text style={{ fontSize: 18, fontFamily: 'Roboto-Medium' }}>
+            Hello Buğra Öner
           </Text>
           <TouchableOpacity onPress={() => navigation.openDrawer()}>
             <ImageBackground
               source={require('../assets/images/user-profile.jpg')}
-              style={{width: 35, height: 35}}
-              imageStyle={{borderRadius: 25}}
+              style={{ width: 35, height: 35 }}
+              imageStyle={{ borderRadius: 25 }}
             />
           </TouchableOpacity>
         </View>
@@ -63,7 +63,7 @@ export default function HomeScreen({navigation}) {
             name="search"
             size={20}
             color="#C6C6C6"
-            style={{marginRight: 5}}
+            style={{ marginRight: 5 }}
           />
           <TextInput placeholder="Search" />
         </View>
@@ -74,11 +74,11 @@ export default function HomeScreen({navigation}) {
             flexDirection: 'row',
             justifyContent: 'space-between',
           }}>
-          <Text style={{fontSize: 18, fontFamily: 'Roboto-Medium'}}>
+          <Text style={{ fontSize: 18, fontFamily: 'Roboto-Medium' }}>
             Upcoming Games
           </Text>
-          <TouchableOpacity onPress={() => {}}>
-            <Text style={{color: '#0aada8'}}>See all</Text>
+          <TouchableOpacity onPress={() => { }}>
+            <Text style={{ color: '#0aada8' }}>See all</Text>
           </TouchableOpacity>
         </View>
 
@@ -93,7 +93,7 @@ export default function HomeScreen({navigation}) {
           loop={true}
         />
 
-        <View style={{marginVertical: 20}}>
+        <View style={{ marginVertical: 20 }}>
           <CustomSwitch
             selectionMode={1}
             option1="Free to play"
